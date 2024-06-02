6304c507a177353aa31dae228bdb8131835b2068
Kimber Ogden <mx.kimber@gmail.com>
2024-06-01 23:12:18 -0500

    Add show, update, and destroy actions to Users controller with error handling/custom messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,45 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
 
   def index
     @users = User.all
     render :index
   end
 
+  def show
+    if @user
+      render :show
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
+
   def create
     @user = User.new(user_params)
 
     if @user.save
+      render :show, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
       render :show
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-
+  def destroy
+    if @user
+      @user.destroy
+      render json: { message: "User destroyed successfully" }
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
 
   private
 
@@ -30,5 +54,8 @@ class UsersController < ApplicationController
       :seller
     )
   end
-end
 
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+end
