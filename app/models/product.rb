14aebbdcf3f8bfa38dbb9c293caeb658ef6bcf20
Kimber Ogden <mx.kimber@gmail.com>
2024-06-04 22:56:46 -0500

    Update associations between user, product, and shop with validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  has_and_belongs_to_many :shops
+
+  validates :product_name, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,6 @@
 class Shop < ApplicationRecord
   belongs_to :user
+  has_and_belongs_to_many :products
+
+  validates :shop_name, presence: true
 end
