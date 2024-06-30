3c324cec70b1579678833172580838ba1e120d5d
Kimber Ogden <mx.kimber@gmail.com>
2024-06-30 13:47:07 -0500

    Add dependent destroy to user products association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  
   has_many :shops, dependent: :destroy
-  has_many :products
+  has_many :products, dependent: :destroy # Add dependent: :destroy
 end
